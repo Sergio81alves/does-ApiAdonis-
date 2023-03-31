4db1440b2d9f6c1bd5081cabb389a19da7eed19b
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-03-31 01:17:53 -0300

    ext NameImage

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,4 +1,4 @@
-//import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
@@ -7,13 +7,21 @@ import Moment from 'App/Models/Moment'
 import { Application } from '@adonisjs/core/build/standalone'
 
 export default class MomentsController {
+  private validationOptions = {
+    type: ['image'],
+    size: '2mb',
+  }
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
 
     const moment = await Moment.create(body)
 
-    const image = request.file('image')
+    /*ele valida a imagem antes de colocar no sistema */
+    const image = request.file('image', this.validationOptions)
 
+    if(image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+    }
     response.status(201)
     return {
       message: 'Momento criado com sucesso!',
